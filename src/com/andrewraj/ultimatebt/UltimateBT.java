fe82445391879de1d9c504a2479916133f5b5e3e
Andrew Raj <me@andrewraj.com>
2012-04-17 00:22:21 +0800

    Updated application and images file names

--- src/com/andrewraj/ultimatebt/UltimateBT.java
+++ src/com/andrewraj/ultimatebt/UltimateBT.java
@@ -3,7 +3,7 @@ package com.andrewraj.ultimatebt;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class UltimateBTActivity extends Activity {
+public class UltimateBT extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
