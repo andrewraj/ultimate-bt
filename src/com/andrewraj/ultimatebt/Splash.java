7011c992dbd8d799b66d737b569e916f25ab020f
Andrew Raj <me@andrewraj.com>
2012-04-27 00:04:39 +0800

    Added splash screen

--- src/com/andrewraj/ultimatebt/Splash.java
+++ src/com/andrewraj/ultimatebt/Splash.java
@@ -8,6 +8,7 @@ package com.andrewraj.ultimatebt;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 
 public class Splash extends Activity {
 
@@ -15,7 +16,7 @@ public class Splash extends Activity {
 	protected void onCreate(Bundle UBT) {
 		// TODO Auto-generated method stub
 		super.onCreate(UBT);
-		
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.splash);
 		
 		Thread timer = new Thread() {
